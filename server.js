19163bdcde4cdde312a886d8680a8607150101e4
Ray C <rayc2045@gmail.com>
2021-05-18 01:14:57 +0800

    Update server.js

--- server.js
+++ server.js
@@ -11,10 +11,18 @@ const io = socketio(server);
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Run when client connects
-io.on('connection', (socket) => console.log(`New WS Connection...`));
+io.on('connection', (socket) => {
+  socket.emit('message', 'Welcome to chat!');
+
+  // Broadcast when a user connects
+  socket.broadcast.emit('message', 'A user has joined the chat');
+
+  // Runs when client disconnects
+  socket.on('disconnect', () => io.emit('message', 'A user has left the chat'));
+});
 
 const PORT = 3000 || process.env.PORT;
 
 server.listen(PORT, () =>
-  console.log(`Server running on http://localhost:${PORT}`)
+  console.log(`Server running on "http://localhost:${PORT}"`)
 );
